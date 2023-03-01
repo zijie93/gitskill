377a68cc844538524e865b923351317ce3dc2631
jacky <lzjee1025@gmail.com>
2023-02-28 22:04:35 -0800

    meaning of github project's documents

---
+++ MeaningOfGithubDocuments.cpp
@@ -0,0 +1,19 @@
+Table of meanings for different documents/folders in github
+Name                           Meaning
+conf/configure            documents for configuration
+src/source                source codes of this project
+doc/document              documents of this project
+test/unitest              tests
+tools/utils               relative tools
+lib                       library documents
+app                       applications
+controllers               controllers, often used in MVC web applications
+models                    models, often used in MVC web applications
+views                     views, displays, often used in MVC web applications
+db                        database relative document
+demo/example              example (codes)
+misc                      other items
+include                   head document, used in c/c++ application
+out/build                 output
+third_party/vender        third party documents
+install                   installation relative documents
